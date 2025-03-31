5bf3851d770cf884da2675dfe02db6a508036606
UltiRed8 <84141770+UltiRed8@users.noreply.github.com>
2025-03-31 16:47:42 +0200

    Added start of click detection

---
+++ ARSolarSystem/Assets/Project/Scripts/InteractionTest.cs
@@ -0,0 +1,29 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class InteractionTest : MonoBehaviour
+{
+    [SerializeField] Camera playerCamera = null;
+    [SerializeField] IAA_Player inputs;
+    [SerializeField] InputAction interactAction;
+    [SerializeField] LayerMask layerToDetect;
+
+    private void Awake()
+    {
+        inputs = new IAA_Player();
+        interactAction = inputs.Player.Interact;
+        interactAction.Enable();
+        interactAction.performed += Interact;
+    }
+
+    public void Interact(InputAction.CallbackContext _context)
+    {
+        Vector2 _hitPosition = _context.ReadValue<Vector2>();
+        Ray _ray = playerCamera.ScreenPointToRay(_hitPosition);
+        if (Physics.Raycast(_ray, out RaycastHit _hit, 1000.0f, layerToDetect))
+        {
+            Debug.Log("Detected: " + _hit.rigidbody.gameObject.name);
+            Destroy(_hit.rigidbody.gameObject);
+        }
+    }
+}
